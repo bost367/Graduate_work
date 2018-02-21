837d817db2ad0d19ef0652b540dbb1a046484fc8
bost367 <kruglovstepan367@gmaol.com>
2018-02-21 23:31:04 +0300

    change buffer

--- make_sin.c
+++ make_sin.c
@@ -5,13 +5,13 @@
 #include <stdlib.h>
 
 #define S_RATE  (96000)
-#define BUF_SIZE (S_RATE*1)
+#define BUF_SIZE (S_RATE*0.3)
 
 int main(int argc, char* argv[])
 {
     if (argc <= 1) return 0;
     int f = atoi(argv[1]);
-    short int buffer[BUF_SIZE];
+    short int buffer[(int) BUF_SIZE];
     float amplitude = 32000;
     for (int i = 0; i < BUF_SIZE; i++)
         buffer[i] += (int)(amplitude * sin((float)(2 * M_PI *i* f / S_RATE)));
