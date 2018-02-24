ad405d34b274260ff31b854dcb31b7d29f96e8e0
Stepan <kruglovstepan367@gmail.com>
2018-02-24 20:13:45 +0700

    wav file is not needed anymore

--- fft.c
+++ fft.c
@@ -1,12 +1,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-
 #include <math.h>
 #ifndef M_PI
 	#define M_PI 3.14159265358979323846
 #endif
-
 #include "fft.h"
 //----------------------------------------------------------------------------
 // функция расчёта поворотных множителей для БПФ
--- fft_an.c
+++ fft_an.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h> // for memcmp
 #include <stdint.h> // for int16_t and int32_t
+#define _USE_MATH_DEFINES
 #include <math.h>
 #include "fft.h"
 #include "wavr.h"
+#ifndef M_PI
+	#define M_PI 3.14159265358979323846
+#endif
 
-void calcFFT(long int* size, int* p, double* freequency, long* in, FILE* logfile)
+#define BUF_TYPE unsigned char
+#define SAMPLE_RATE (96000) //96000
+#define BUF_SIZE (65536)
+#define DEGREE (16)
+
+void calcFFT(int size_array, int p2, int delta, BUF_TYPE* in, FILE* logfile)
 {
-	long int size_array = *size;
-	int p2 = *p;
-	double delta = *freequency;
 	float *c;
 	float *out;
 
-	c = calloc(size_array * 2, sizeof(float));
-	out = calloc(size_array * 2, sizeof(float));
+	c = calloc(size_array * 2, sizeof(BUF_TYPE));
+	out = calloc(size_array * 2, sizeof(BUF_TYPE));
 
 	fft_make(p2, c);// функция расчёта поворотных множителей для БПФ
 	fft_calc(p2, c, in, out, 1);
@@ -75,29 +80,9 @@ void fillArray(float* in, const int* size_array, FILE* wav) {
 
 int main(int argc, char *argv[])
 {
-	char* filename = argv[1];
-	FILE*  wav = fopen(filename, "rb");
-	struct wavfile header;
-
-	if (wav == NULL) {
-		fprintf(stderr, "Can't open input file %s\n", filename);
-		exit(1);
-	}
-
-	if (fread(&header, sizeof(header), 1, wav) < 1) {
-		fprintf(stderr, "Can't read input file header %s\n", filename);
-		exit(1);
-	}
-
-	long int size_array;
-	int p2;
-	double frequency;
-	check(&header, filename);
-
-	getWavInfo(&size_array, &p2, &frequency, &header);
-
-	float* in = calloc(size_array * 2, sizeof(float));
-	fillArray(in, &size_array, wav);
+	BUF_TYPE* buffer;
+	buffer = calloc(BUF_SIZE, sizeof(BUF_TYPE));
+	writeSinForFFT(buffer);
 
 	FILE* logfile;
 	logfile = fopen("test.txt", "w+");
@@ -106,22 +91,41 @@ int main(int argc, char *argv[])
 		return 0;
 	}
 
-	calcFFT(&size_array, &p2, &frequency, in, logfile);
+	calcFFT((int) (BUF_SIZE / 2), DEGREE, 1/SAMPLE_RATE, buffer, logfile);
 
-	fclose(wav);
 	fclose(logfile);
-	free(in);
+	free(buffer);
 	exit(0);
 }
 
-void getWavInfo(long int* size_array, int* p, double* freequency, const struct wavfile* header)
-{
-	print_header_info(header);
-	printf("\nchunk=%d\n", header->bytes_in_data / header->bytes_by_capture);
-	//показатель двойки массива
-	*p = (int) (log2(header->bytes_in_data / header->bytes_by_capture));
-	printf("log2=%d\n", p);
-	*size_array = 1 << (int)(log2(header->bytes_in_data / header->bytes_by_capture));
-	printf("size array=%ld \n", size_array);
-	*freequency = ((float) header->frequency) / (float) *size_array;
+void writeSinForFFT(BUF_TYPE* buffer) {
+	int f = 1300;
+	int amplitude = 125;
+	for (int i = 0; i < (int) (BUF_SIZE / 2); i++) {
+		*buffer += (BUF_TYPE)((amplitude * sin((float)(2.0 * M_PI * i * f / SAMPLE_RATE))) + amplitude);
+		buffer += 2;
+	}
+}
+
+void writeUsualSin() {
+	int f = 1300;
+	BUF_TYPE buffer[(int)BUF_SIZE];
+	int amplitude = 125;
+
+	for (int i = 0; i < BUF_SIZE; i++) {
+		buffer[i] += (BUF_TYPE)((amplitude * sin((float)(2.0 * M_PI * i * f / SAMPLE_RATE))) + amplitude);
+	}
+
+	FILE* logfile;
+	logfile = fopen("test.txt", "w+");
+	if (!logfile) {
+		printf("Failed open file, error\n");
+		return 0;
+	}
+
+	float cur_freq = 0;
+	for (int i = 0; i < BUF_SIZE; i++) {
+		fprintf(logfile, "%.6f %d.0\n", cur_freq, buffer[i]);
+		cur_freq += (float) 1.0 / SAMPLE_RATE;
+	}
 }
